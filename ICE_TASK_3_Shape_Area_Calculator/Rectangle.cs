609ea5dc20d318540f0e15e2414581616c4c6f08
ST10439783 <ST10439783@vcconnect.edu.za>
2025-03-13 16:28:59 +0200

    3rd Commit Added Class Rectangle

---
+++ ICE_TASK_3_Shape_Area_Calculator/Rectangle.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_TASK_3_Shape_Area_Calculator
+{
+    class Rectangle : Shape, ICalculateArea
+    {
+        public double Length { get; }
+        public double Width { get; }
+
+        public Rectangle(string name, double length, double width) : base(name)
+        {
+            this.Length = length;
+            this.Width = width;
+        }
+
+        public double CalculateArea()
+        {
+            double temp = 0.0;
+
+            temp = Length * Width;
+
+            return temp;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine(CalculateArea().ToString("0.##"));
+        }
+    }
+}
