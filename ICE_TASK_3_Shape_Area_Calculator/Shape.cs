3e0110a116f3b65391f1b002a5a138e4ce9ad2b2
ST10439783 <ST10439783@vcconnect.edu.za>
2025-03-13 16:26:46 +0200

    2nd Commit Added Abstract Class Shape

---
+++ ICE_TASK_3_Shape_Area_Calculator/Shape.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_TASK_3_Shape_Area_Calculator
+{
+    abstract class Shape
+    {
+        public string Name { get; }
+
+        public Shape(string name)
+        {
+            this.Name = name;
+        }
+
+        public virtual void Display()
+        {
+            Console.WriteLine("Name ~ " + Name);
+        }
+    }
+}
