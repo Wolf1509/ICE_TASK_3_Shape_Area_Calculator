93fae14b561e84302cc04f6295e8615d8de3ce56
ST10439783 <ST10439783@vcconnect.edu.za>
2025-03-13 16:25:08 +0200

    1st Commit Added Interface ICalculateArea

---
+++ ICE_TASK_3_Shape_Area_Calculator/ICalculateArea.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ICE_TASK_3_Shape_Area_Calculator
+{
+    interface ICalculateArea
+    {
+        double CalculateArea();
+    }
+}
